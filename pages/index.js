4b53e5225fcdb85b2ed547807743f173e432aac6
uchikori <ucchii.wa@gmail.com>
2023-02-22 17:47:40 +0900

    ✨

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,20 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '@/styles/Home.module.css'
-import { Hero } from '@/components/Hero'
-import { Container } from '@/components/Container'
-import { Meta } from '@/components/Meta'
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "@next/font/google";
+import styles from "@/styles/Home.module.css";
+import { Hero } from "@/components/Hero";
+import { Container } from "@/components/Container";
+import { Meta } from "@/components/Meta";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
-export default function Home(){
+export default function Home() {
   return (
     <>
       <Container>
-        
         <Meta />
-        <Hero title="CUBE" subtitle="アウトプットしていくサイト" imageOn />
+        <Hero title="CUBE" subtitle="アウトプットしていく" imageOn />
       </Container>
     </>
-  )
-} 
+  );
+}
