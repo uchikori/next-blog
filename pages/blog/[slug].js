73c28275eaca6f78db046dd00180ee1133045aed
uchikori <ucchii.wa@gmail.com>
2023-02-25 10:46:28 +0900

    💄

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,31 +1,29 @@
 import { Container } from "@/components/Container";
 import { useRouter } from "next/router";
 
-export default function Page(props){
-
+export default function Page(props) {
   const { category } = props;
 
   return (
     <Container>
-      <h1>{ category }</h1>
+      <h1>{category}</h1>
     </Container>
-    
-  )
+  );
 }
 
-export async function getStaticPaths(){
+export async function getStaticPaths() {
   return {
-    paths:[
-      {params:{ slug: 'schedule' }},
-      {params:{ slug: 'music' }},
+    paths: [
+      // {params:{ slug: 'schedule' }},
+      { params: { slug: "music" } },
     ],
     fallback: false,
-  }
+  };
 }
-export async function getStaticProps(context){
-// export async function getServerSideProps(context){
-  const pageSlug = context.params.slug
+export async function getStaticProps(context) {
+  // export async function getServerSideProps(context){
+  const pageSlug = context.params.slug;
   return {
-    props: { category: pageSlug }
-  }
+    props: { category: pageSlug },
+  };
 }
