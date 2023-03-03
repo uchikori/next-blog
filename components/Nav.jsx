97ade27fc5a445e092be63f4b45cf5d7d7f6ba7f
uchikori <ucchii.wa@gmail.com>
2023-03-03 15:52:03 +0900

    ✅

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,19 +1,51 @@
 import Link from "next/link";
 import styles from "@/styles/Nav.module.css";
+import { useState } from "react";
 
 export const Nav = () => {
+  const [navIsOpen, setNavIsOpen] = useState(false);
+
+  const toggleNav = () => {
+    setNavIsOpen((navIsOpen) => !navIsOpen);
+  };
+  const closeNav = () => {
+    setNavIsOpen(false);
+  };
   return (
     <>
-      <nav>
+      <nav className={navIsOpen ? styles.open : styles.close}>
+        {navIsOpen && (
+          <style jsx global>
+            {`
+              @media (max-width: 767px) {
+                body {
+                  overflow: hidden;
+                  position: fixed;
+                  wodth: 100%;
+                }
+              }
+            `}
+          </style>
+        )}
+        <button onClick={toggleNav} className={styles.btn}>
+          <span className={styles.bar}></span>
+          <span className="sr-only">MENU</span>
+        </button>
         <ul className={styles.list}>
           <li>
-            <Link href="/">HOME</Link>
+            <Link href="/" onClick={closeNav}>
+              HOME
+            </Link>
           </li>
           <li>
-            <Link href="/about">ABOUT</Link>
+            <Link href="/about" onClick={closeNav}>
+              ABOUT
+            </Link>
           </li>
           <li>
-            <Link href="/blog">BLOG</Link>
+            <Link href="/blog" onClick={closeNav}>
+              BLOG
+            </Link>
           </li>
         </ul>
       </nav>
--- pages/blog/category/[slug].js
+++ pages/blog/category/[slug].js
@@ -1,4 +1,5 @@
 import { Container } from "@/components/Container";
+import { Meta } from "@/components/Meta";
 import { PostHeader } from "@/components/post-header";
 import { Posts } from "@/components/posts";
 import { getAllCategories, getAllPostsByCategory } from "@/lib/client";
@@ -7,9 +8,9 @@ import { getPlaiceholder } from "plaiceholder";
 
 export default function Category(props) {
   const { name, posts } = props;
-  console.log(posts);
   return (
     <Container>
+      <Meta pageTitle={name} pageDesc={`${name}に関する記事`} />
       <PostHeader title={name} subtitle="Blog Category" />
       <Posts posts={posts} />
     </Container>
@@ -25,14 +26,22 @@ export async function getStaticPaths() {
     fallback: false,
   };
 }
+
 export async function getStaticProps(context) {
+  //1:getStaticPathsからパラメータ取得
   const categorySlug = context.params.slug;
+
+  //2:全カテゴリーを取得
   const allCategories = await getAllCategories();
 
+  //3:全カテゴリーのスラッグとパラメータのスラッグ名称が等しい場合にcategoryにオブジェクトを格納
   const category = allCategories.find(({ slug }) => slug === categorySlug);
 
+  //4:3で取得したオブジェクトの中からidを取得して渡し記事を取得する
   const posts = await getAllPostsByCategory(category.id);
 
+  //5:取得した記事の中に「eyecatch」が設定されていない場合はローカルのアイキャッチを読み込ませる
+  //ベース64でエンコードした画像を渡す
   for (const post of posts) {
     if (!post.hasOwnProperty("eyecatch")) {
       post.eyecatch = eyecatchLocal;
