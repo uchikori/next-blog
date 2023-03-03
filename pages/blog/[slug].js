9c9863c54dd25d906073aa918eb9da2ff80045ce
uchikori <ucchii.wa@gmail.com>
2023-03-03 13:41:28 +0900

    ♻️

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -46,6 +46,7 @@ export default function Post(props) {
 
         <figure>
           <Image
+            key={eyecatch.url}
             src={eyecatch.url}
             alt=""
             width={eyecatch.width}
